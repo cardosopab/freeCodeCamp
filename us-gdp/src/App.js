a33e6c391583465b316e4e9d21330a4005cfe84a
Pablo <cardosopab@gmail.com>
2023-07-25 04:23:47 -0500

    basic BarGraph component example.

--- us-gdp/src/App.js
+++ us-gdp/src/App.js
@@ -1,9 +1,20 @@
-function App() {
+import BarGraph from './components/BarGraph'
+
+export default function App() {
+  const data = [
+    { letter: "A", frequency: 0.08167 },
+    { letter: "B", frequency: 0.01492 },
+    { letter: "C", frequency: 0.02782 },
+    { letter: "D", frequency: 0.04253 },
+    { letter: "E", frequency: 0.12702 },
+    { letter: "F", frequency: 0.02288 },
+    { letter: "G", frequency: 0.02015 },
+    { letter: "H", frequency: 0.06094 },
+  ]
+
   return (
-    <>
-      <h1>Hello, World!</h1>
-    </>
+    <div>
+      <BarGraph data={data} />
+    </div>
   );
 }
-
-export default App;
